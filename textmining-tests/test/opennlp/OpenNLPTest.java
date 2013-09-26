f94eb5a0e7336d231441644e1906d7c21edca231
mail@thomaseger.de <mail@thomaseger.de>
2013-09-26 11:56:59 +0200

    Task#2: Added tests for NameFinder and PartOfSpeechTagger API.

--- textmining-tests/test/opennlp/OpenNLPTest.java
+++ textmining-tests/test/opennlp/OpenNLPTest.java
@@ -1,16 +1,25 @@
 package opennlp;
 
-import static org.hamcrest.core.Is.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinder;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTagger;
+import opennlp.tools.postag.POSTaggerME;
 import opennlp.tools.sentdetect.SentenceDetector;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.Span;
 
 import org.junit.Test;
 
@@ -48,7 +57,18 @@ public class OpenNLPTest {
 	
 	@Test
 	public void Name_Finder_API () throws Exception {
-		fail();
+		//Arrange
+		File modelFile = new File(MODELS, "en-ner-person.bin");
+		TokenNameFinderModel model = new TokenNameFinderModel(modelFile);
+		TokenNameFinder finder = new NameFinderME(model);
+		String[] tokens = new String[]{"My", "name", "is", "Thomas", "."};
+		
+		//Act
+		Span[] findings = finder.find(tokens);
+		
+		//Assert
+		String firstName = tokens[findings[0].getStart()];
+		assertThat(firstName, is(equalTo("Thomas")));	
 	}
 	
 	@Test
@@ -58,7 +78,15 @@ public class OpenNLPTest {
 	
 	@Test
 	public void Part_of_Speech_Tagger_API () throws Exception {
-		fail();
+		File modelFile = new File(MODELS, "en-pos-maxent.bin");
+		String[] tokens = new String[]{"My", "name", "is", "Thomas", "."};
+		String[] expectedTags = {"PRP$", "NN", "VBZ", "NNP", "."};
+		POSModel model = new POSModel(modelFile);
+		POSTagger tagger = new POSTaggerME(model);
+	
+		String[] tags = tagger.tag(tokens);
+		
+		assertThat(tags, is(equalTo(expectedTags)));
 	}
 	
 	@Test
